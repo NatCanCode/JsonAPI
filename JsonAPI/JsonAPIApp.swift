5789356ea1d6ac14f66a25e2b50d148a4197b0b5
NatCanCode <nathalie.piat1@gmail.com>
2022-09-27 10:24:11 +0200

    Initial Commit

---
+++ JsonAPI/JsonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAPIApp.swift
+//  JsonAPI
+//
+//  Created by N N on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct JsonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
